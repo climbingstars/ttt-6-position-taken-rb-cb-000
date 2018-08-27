d1dcfcbf0a90322983bce3fe081f3a3635d0daaa
Kyle Sanders <kyle.sanders.315@gmail.com>
2018-08-27 00:40:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if index == "" || " " || nil
+  if board == "" || " " || nil
     false
   elsif index == "X" || "O"
     true
